2954de92fe30d23427ca5be0bf15b97324db8a64
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-04 17:56:06 -0300

    Resultado após vídeo 1.3

--- setup/urls.py
+++ setup/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('galeria.urls')),
+    path('', include('usuarios.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
