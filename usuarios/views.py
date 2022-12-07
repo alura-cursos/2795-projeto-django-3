0f2b0afebc0ce3939c040933d44a052417012c4d
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-07 16:00:06 -0300

    Resultado após vídeo 3.4

--- usuarios/views.py
+++ usuarios/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 
 from django.contrib import auth
 
+from django.contrib import messages
+
 def login(request):
     form = LoginForms()
 
@@ -23,8 +25,10 @@ def login(request):
         )
         if usuario is not None:
             auth.login(request, usuario)
+            messages.success(request, f'{nome} logado com sucesso!')
             return redirect('index')
         else:
+            messages.error(request, 'Erro ao efetuar login')
             return redirect('login')
 
     return render(request, 'usuarios/login.html', {'form': form})
@@ -37,6 +41,7 @@ def cadastro(request):
 
         if form.is_valid():
             if form["senha_1"].value() != form["senha_2"].value():
+                messages.error(request, 'Senhas não são iguais')
                 return redirect('cadastro')
 
             nome=form['nome_cadastro'].value()
@@ -44,6 +49,7 @@ def cadastro(request):
             senha=form['senha_1'].value()
 
             if User.objects.filter(username=nome).exists():
+                messages.error(request, 'Usuário já existente')
                 return redirect('cadastro')
 
             usuario = User.objects.create_user(
@@ -52,6 +58,7 @@ def cadastro(request):
                 password=senha
             )
             usuario.save()
+            messages.success(request, 'Cadastro efetuado com sucesso!')
             return redirect('login')
 
     return render(request, 'usuarios/cadastro.html', {'form': form})
