3c30365f53ceca2b52ba33f03907de1cff51d884
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-07 15:44:09 -0300

    Resultado após vídeo 3.2

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 
 from usuarios.forms import LoginForms, CadastroForms
 
+from django.contrib.auth.models import User
+
 def login(request):
     form = LoginForms()
     return render(request, 'usuarios/login.html', {'form': form})
@@ -12,9 +14,23 @@ def cadastro(request):
     if request.method == 'POST':
         form = CadastroForms(request.POST)
 
-        if form["senha_1"].value() != form["senha_2"].value():
-            return redirect('cadastro')
+        if form.is_valid():
+            if form["senha_1"].value() != form["senha_2"].value():
+                return redirect('cadastro')
 
-            
+            nome=form['nome_cadastro'].value()
+            email=form['email'].value()
+            senha=form['senha_1'].value()
+
+            if User.objects.filter(username=nome).exists():
+                return redirect('cadastro')
+
+            usuario = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+            usuario.save()
+            return redirect('login')
 
     return render(request, 'usuarios/cadastro.html', {'form': form})
