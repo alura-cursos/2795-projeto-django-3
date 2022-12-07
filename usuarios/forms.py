49ae64495514b2aed4058ffa3d7ac68e7d77f442
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-07 09:18:44 -0300

    Resultado após vídeo 2.2

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -5,10 +5,21 @@ class LoginForms(forms.Form):
         label='Nome de Login', 
         required=True, 
         max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex.: João Silva',
+            }
+        )
     )
     senha=forms.CharField(
         label='Senha', 
         required=True, 
         max_length=70,
-        widget=forms.PasswordInput(),
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite a sua senha',
+            }
+        ),
     )
