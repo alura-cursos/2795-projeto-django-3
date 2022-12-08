a076837bbad02edb38d72e3e9797066cb51a2780
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-08 09:44:04 -0300

    Resultado após vídeo 5.1

--- setup/settings.py
+++ setup/settings.py
@@ -138,3 +138,11 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# messages
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success'
+}
