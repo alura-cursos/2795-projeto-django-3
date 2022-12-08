30524a77be6d613b3d8c3798a1db15fc36615b60
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-08 09:21:07 -0300

    Resultado após vídeo 4.2

--- galeria/admin.py
+++ galeria/admin.py
@@ -6,7 +6,7 @@ class ListandoFotografias(admin.ModelAdmin):
     list_display = ("id", "nome", "legenda", "publicada")
     list_display_links = ("id","nome")
     search_fields = ("nome",)
-    list_filter = ("categoria",)
+    list_filter = ("categoria", 'usuario')
     list_editable = ("publicada",)
     list_per_page = 10
 
--- galeria/models.py
+++ galeria/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from datetime import datetime
 
+from django.contrib.auth.models import User
+
 class Fotografia(models.Model):
 
     OPCOES_CATEGORIA = [
@@ -18,6 +20,13 @@ class Fotografia(models.Model):
     foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
     publicada = models.BooleanField(default=False)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
+    usuario = models.ForeignKey(
+        to=User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name='user'
+    )
 
     def __str__(self):
         return self.nome
